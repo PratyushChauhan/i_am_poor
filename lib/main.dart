c8d0c3fce4b0bef9a8919501ec4f3a098e0ea347
Pratyush Chauhan <pratyushchauhan62@gmail.com>
2022-08-15 14:25:17 +0530

    Made a new app with icons images and appbar.

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,19 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MaterialApp(
-    builder: ,
+  runApp(MaterialApp(
+    home: Scaffold(
+      appBar: AppBar(
+        title: const Center(child: Text("I Am Poor")),
+        backgroundColor: Colors.blueGrey[800],
+      ),
+      body: const Center(
+        child: Image(
+          image: AssetImage(
+              "Images/stone.png"), //img src  <a target="_blank" href="https://icons8.com/icon/TOf5ZHQ4bJhe/coal">Coal</a> icon by <a target="_blank" href="https://icons8.com">Icons8</a>
+        ),
+      ),
+      backgroundColor: Colors.blueGrey[500],
+    ),
   ));
 }
